8d39fa070d14379003e2fc60d33d810798b442e1
Liangqin Sun <liansun@microsoft.com>
2020-05-25 01:18:28 -0700

    Initial check-in

---
+++ src/extension.ts
@@ -0,0 +1,69 @@
+import * as vscode from 'vscode';
+
+const opn = require("opn");
+
+class Rule {
+    constructor(public readonly pattern: string, public readonly list: string[]) {
+    }
+
+    match(file: string) {
+        return file.match(this.pattern);
+    }
+
+    expand(file: string) {
+        return this.list.map(x => {
+            return file.replace(new RegExp(this.pattern), x);
+        });
+    }
+}
+
+class Configuration {
+    static Load() {
+        const rules = vscode.workspace.getConfiguration().get<Rule[]>(
+            'quickBrowse.rules', []);
+        return rules.map(x => new Rule(x.pattern, x.list));
+    }
+}
+
+class UrlQuickPickItem implements vscode.QuickPickItem {
+    constructor(public readonly label: string, public readonly url: string) {
+    }
+}
+
+async function selectUrlFromPick(urls: string[]) {
+    const urlQuickPickItems = urls.map(x => {
+        return new UrlQuickPickItem('$(file-symlink-file)  ' + x, x);
+    });
+
+    const selectedUrl = await vscode.window.showQuickPick(urlQuickPickItems,
+        { placeHolder: 'Browse url' });
+    return selectedUrl?.url;
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(vscode.commands.registerCommand('quickBrowse.browse', async () => {
+        const activeTextEditor = vscode.window.activeTextEditor;
+        if (!activeTextEditor) {
+            return;
+        }
+        const currentFilePath = activeTextEditor.document.fileName;
+
+        const rules = Configuration.Load();
+        const matchedRule = rules.find(x => x.match(currentFilePath))
+        const urls = matchedRule?.expand(currentFilePath) || [];
+
+        let url;
+        if (urls.length == 0) {
+            return;
+        } else if (urls.length == 1) {
+            url = urls[0];
+        } else {
+            url = await selectUrlFromPick(urls);
+            if (!url) {
+                return;
+            }
+        }
+
+        opn(url);
+    }));
+}
